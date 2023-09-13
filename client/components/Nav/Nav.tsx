b9c0096e2062c09a5fc4ac36b56d5c380a197ec7
Ryan Nguyen <136216708+ryananhtuan-nguyen@users.noreply.github.com>
2023-09-13 19:51:12 +1200

    fixed Neils mistake

--- client/components/Nav/Nav.tsx
+++ client/components/Nav/Nav.tsx
@@ -1,12 +1,12 @@
+import { useAuth0 } from '@auth0/auth0-react'
 import { useNavigate } from 'react-router-dom'
-
 
 interface Props {
   toggleMenu: () => void
 }
 
 function Nav(props: Props) {
- 
+  const { isAuthenticated, logout } = useAuth0()
   const navigate = useNavigate()
 
   // function handleLogin() {
--- server/db/wardrobe.ts
+++ server/db/wardrobe.ts
@@ -1,29 +1,29 @@
 import connection from './connection.ts'
 import db from './connection.ts'
 
-export async function getAllwardrobe(userId: string) {
-  return await db('wardrobe').where('user_id', userId).select()
-}
+// export async function getAllwardrobe(userId: string) {
+//   return await db('wardrobe').where('user_id', userId).select()
+// }
 
-export function addFruit(fruit: FruitSnakeCase, db = connection) {
-  return db('wardrobe').insert(fruit)
-}
+// export function addFruit(fruit: FruitSnakeCase, db = connection) {
+//   return db('wardrobe').insert(fruit)
+// }
 
-export function updateFruit(newFruit: FruitSnakeCase, db = connection) {
-  return db('wardrobe').where('id', newFruit.id).update(newFruit)
-}
+// export function updateFruit(newFruit: FruitSnakeCase, db = connection) {
+//   return db('wardrobe').where('id', newFruit.id).update(newFruit)
+// }
 
-export function deleteFruit(id: number, db = connection) {
-  return db('wardrobe').where('id', id).delete()
-}
+// export function deleteFruit(id: number, db = connection) {
+//   return db('wardrobe').where('id', id).delete()
+// }
 
-export function userCanEdit(fruitId: number, auth0Id: string, db = connection) {
-  return db('wardrobe')
-    .where('id', fruitId)
-    .first()
-    .then((fruit: FruitSnakeCase) => {
-      if (fruit.added_by_user !== auth0Id) {
-        throw new Error('Unauthorized')
-      }
-    })
-}
+// export function userCanEdit(fruitId: number, auth0Id: string, db = connection) {
+//   return db('wardrobe')
+//     .where('id', fruitId)
+//     .first()
+//     .then((fruit: FruitSnakeCase) => {
+//       if (fruit.added_by_user !== auth0Id) {
+//         throw new Error('Unauthorized')
+//       }
+//     })
+// }
