b033a59c852129bed3946cfab30a658438054ab9
Youn Doh <youndoh@gmail.com>
2023-09-14 20:28:41 +1200

    fixing css and fonts

--- client/Pages/MyWardrobe/MyWardrobe.tsx
+++ client/Pages/MyWardrobe/MyWardrobe.tsx
@@ -5,7 +5,6 @@ import WardrobeList from '../../components/WardrobeList/WardrobeList'
 function MyWardrobe() {
   const { isAuthenticated, getAccessTokenSilently } = useAuth0()
   const { data, isLoading, mutationDel } = useMyWardrobeHook()
-  // console.log(data)
 
   async function handleDeleteItem(id: number) {
     const token = await getAccessTokenSilently()
@@ -15,25 +14,36 @@ function MyWardrobe() {
 
   return (
     <>
-      {isLoading && <div>Loading ...</div>}
-      <h1>My Wardrobe</h1>
+      {' '}
+      <div className="flex flex-col items-center w-[100vw]">
+        {isLoading && <div>Loading ...</div>}
+        <h1 className="m-20 text-4xl border-b-[5px]">My Wardrobe</h1>
+        <div className="flex flex-row justify-center place-content-evenly w-96 border-4 text-2xl mb-[40px] ">
+          <p className="m-6">CATEGORY</p>
+          <p className="m-6">STYLE</p>
+        </div>
+      </div>
       {isAuthenticated && (
         <ul>
-          {data &&
-            data?.length > 0 &&
-            data.map((item) => (
-              <li className="list-none flex gap-4" key={item.id}>
-                <WardrobeList
-                  wardrobe={item}
-                  handleDeleteItem={handleDeleteItem}
-                />
-              </li>
-            ))}
-          {data && data?.length == 0 && (
-            <li>
-              <h3>Wardrobe is empty!</h3>
-            </li>
-          )}
+          <div className="grid col-auto place-content-center">
+            <div className="grid grid-cols-4 justify-center">
+              {data &&
+                data?.length > 0 &&
+                data.map((item) => (
+                  <li className="list-none flex justfy-center" key={item.id}>
+                    <WardrobeList
+                      wardrobe={item}
+                      handleDeleteItem={handleDeleteItem}
+                    />
+                  </li>
+                ))}
+              {data && data?.length == 0 && (
+                <li>
+                  <h3>Wardrobe is empty!</h3>
+                </li>
+              )}
+            </div>
+          </div>
         </ul>
       )}
     </>
