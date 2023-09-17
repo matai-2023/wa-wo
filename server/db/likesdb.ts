7c4a7bade277f896107f39e494ba846d85a3224d
neilsen-ngoi <112848446+neilsen-ngoi@users.noreply.github.com>
2023-09-17 21:44:56 +1200

    likestest

--- server/db/likesdb.ts
+++ server/db/likesdb.ts
@@ -1,5 +1,19 @@
 import connection from './connection.ts'
 
-export async function toggleLike() {}
+// interface Like {
+//   user_id: string
+//   outfit_id: number
+// }
+export async function addLike(
+  userId: string,
+  outfitId: number,
+  db = connection
+) {
+  await db('likes').insert({
+    user_id: userId,
+    outfit_id: outfitId,
+  })
+}
+export async function removeLike(userId: string, db = connection) {}
 
 export async function countLikes() {}
