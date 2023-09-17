e0e5af682d0ef51b631fcde1615d880883a513b3
neilsen-ngoi <112848446+neilsen-ngoi@users.noreply.github.com>
2023-09-17 19:17:05 +1200

    fixed delete friend

--- client/Pages/FriendList/FriendList.tsx
+++ client/Pages/FriendList/FriendList.tsx
@@ -25,34 +25,29 @@ function FriendList() {
 
   return (
     <>
-          <h1 className="flex  justify-center font-bold text:[25px] md:text-[30px] mb-10">
-            My friends
-          </h1>
-    <div className='flex mb-[20px] justify-center'>
-      <div className="flex w-[300px] md:w-[600px] border-4 border-orange pt-10 pb-10 h-auto rounded-lg flex-col items-center justify-center">
-
+      <h1 className="flex  justify-center font-bold text:[25px] md:text-[30px] mb-10">
+        My friends
+      </h1>
+      <div className="flex mb-[20px] justify-center">
+        <div className="flex w-[300px] md:w-[600px] border-4 border-orange pt-10 pb-10 h-auto rounded-lg flex-col items-center justify-center">
           <div className="flex flex-col justify-center text-orange">
             <ul className="m-4">
               {data &&
                 data.length > 0 &&
                 data?.map((friend: any) => (
                   <li
-
                     key={friend.nickname}
                     className="list-none flex flex-col items-center text-xl text-blue-300 mb-6 hover:text-orange hover:text-2xl border-2 p-2 rounded-lg"
-                  > 
-                  <div className='text-red-700 mr-4 '>
-                  <AiFillHeart/>
-                </div>
+                  >
+                    <div className="text-red-700 mr-4 ">
+                      <AiFillHeart />
+                    </div>
 
                     <Link to={`/friend/${friend.auth0_id}`}>
                       <h3>{friend.nickname}</h3>
                     </Link>
 
-                    <button
-                      data-testid="testing"
-                      onClick={() => handleDeleteFriend(friend.auth0_id)}
-                    >
+                    <button onClick={() => handleDeleteFriend(friend.auth0_id)}>
                       <Icon className="bg-warning">
                         <i className="fa-solid fa-trash" />
                       </Icon>
