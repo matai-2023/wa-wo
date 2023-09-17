2bf02c1ed47aa25f1d64a8094e444e8f43b41dbc
neilsen-ngoi <112848446+neilsen-ngoi@users.noreply.github.com>
2023-09-17 15:11:15 +1200

    style footer and homepage

--- client/components/Title/Title.tsx
+++ client/components/Title/Title.tsx
@@ -1,16 +1,18 @@
 import { Link } from 'react-router-dom'
-import { GiClothes } from "react-icons/gi";
-import { IfAuthenticated } from '../../apis/Authenticated';
+import { GiClothes } from 'react-icons/gi'
+import { IfAuthenticated } from '../../apis/Authenticated'
 function Title() {
   return (
     <>
       <div>
-    <Link className="lg:text-[70px] pr-14 text-orange text-[40px] top-2]" to="/">
-      <h5 className='mb-[40px]'>WARDROBE WONDERS</h5>
-    </Link>
-        </div>
-        </>
-      
+        <Link
+          className="lg:text-[70px] pr-14 text-orange text-[40px] top-2]"
+          to="/"
+        >
+          <h5 className="ml-2 mb-[40px]">WARDROBE WONDERS</h5>
+        </Link>
+      </div>
+    </>
   )
 }
 
